e44d51123d429702e153d5d029c6f880ca321405
Sirtucx <curtiscabral@gmail.com>
2017-07-21 16:19:50 -0400

    - Added Tucxbot Form for testing - Re-Added the TwitchIRC projects as a proper class library and not a "Standard Library"

---
+++ TwitchIRC/TwitchIRC/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchIRC
+{
+    class Settings
+    {
+        public string Username;
+        public string OAuth;
+    }
+}
---
+++ TwitchIRC/TwitchIRC/Twitch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace TwitchIRC
+{
+    public class Twitch
+    {
+        private static Twitch m_Instance;
+        public static Twitch Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = new Twitch();
+                }
+                return m_Instance;
+            }
+        }
+
+        private bool m_bInitialized;
+        private Settings m_Settings;
+
+        private Twitch()
+        {
+            m_bInitialized = false;
+            string sSettingsPath = Directory.GetCurrentDirectory() + "/Data/Settings.json";
+            if (File.Exists(sSettingsPath))
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(sSettingsPath, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(fs))
+                        {
+                            if (sr.Peek() >= 0)
+                            {
+                                string rawData = sr.ReadToEnd();
+                                m_Settings = JsonConvert.DeserializeObject<Settings>(rawData);
+
+                                if (m_Settings != null)
+                                {
+                                    Console.WriteLine("Username: " + m_Settings.Username);
+                                    Console.WriteLine("OAuth: " + m_Settings.OAuth);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (FileNotFoundException fnfe)
+                {
+                    Console.WriteLine(fnfe.Message);
+                    Console.WriteLine(sSettingsPath);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Required File Settings.json is missing from the Data folder. Please re-add this file here: " + sSettingsPath + " to continue!");
+            }
+        }
+    }
+}
