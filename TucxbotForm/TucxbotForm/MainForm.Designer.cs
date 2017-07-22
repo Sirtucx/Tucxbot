48bfb7c13ba36cfc4d16382475de2d3f78917f88
Sirtucx <curtiscabral@gmail.com>
2017-07-22 16:53:59 -0400

    - Updated the Form to have more control on Chat Output, Whisper Output, Joining and Leaving Channels.

--- TucxbotForm/TucxbotForm/MainForm.Designer.cs
+++ TucxbotForm/TucxbotForm/MainForm.Designer.cs
@@ -31,22 +31,32 @@
             this.btnJoin = new System.Windows.Forms.Button();
             this.btnLeave = new System.Windows.Forms.Button();
             this.tBoxJoin = new System.Windows.Forms.TextBox();
-            this.tBoxLeave = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.rTBoxChat = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.rTBoxEvents = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tBoxWhisper = new System.Windows.Forms.TextBox();
+            this.rBtnWhisper = new System.Windows.Forms.RadioButton();
+            this.rBtnChat = new System.Windows.Forms.RadioButton();
+            this.rTBoxChatOutput = new System.Windows.Forms.RichTextBox();
+            this.cBoxChannels = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cBoxJoinedChannels = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnJoin
             // 
             this.btnJoin.Enabled = false;
-            this.btnJoin.Location = new System.Drawing.Point(13, 69);
+            this.btnJoin.Location = new System.Drawing.Point(7, 48);
             this.btnJoin.Name = "btnJoin";
-            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.Size = new System.Drawing.Size(133, 23);
             this.btnJoin.TabIndex = 0;
+            this.btnJoin.TabStop = false;
             this.btnJoin.Text = "Join";
             this.btnJoin.UseVisualStyleBackColor = true;
             this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
@@ -54,84 +64,179 @@
             // btnLeave
             // 
             this.btnLeave.Enabled = false;
-            this.btnLeave.Location = new System.Drawing.Point(246, 69);
+            this.btnLeave.Location = new System.Drawing.Point(168, 48);
             this.btnLeave.Name = "btnLeave";
-            this.btnLeave.Size = new System.Drawing.Size(75, 23);
+            this.btnLeave.Size = new System.Drawing.Size(133, 23);
             this.btnLeave.TabIndex = 1;
+            this.btnLeave.TabStop = false;
             this.btnLeave.Text = "Leave";
             this.btnLeave.UseVisualStyleBackColor = true;
             this.btnLeave.Click += new System.EventHandler(this.btnLeave_Click);
             // 
             // tBoxJoin
             // 
-            this.tBoxJoin.Location = new System.Drawing.Point(13, 33);
+            this.tBoxJoin.Location = new System.Drawing.Point(7, 22);
             this.tBoxJoin.Name = "tBoxJoin";
-            this.tBoxJoin.Size = new System.Drawing.Size(75, 20);
+            this.tBoxJoin.Size = new System.Drawing.Size(133, 20);
             this.tBoxJoin.TabIndex = 2;
-            // 
-            // tBoxLeave
-            // 
-            this.tBoxLeave.Location = new System.Drawing.Point(245, 33);
-            this.tBoxLeave.Name = "tBoxLeave";
-            this.tBoxLeave.Size = new System.Drawing.Size(75, 20);
-            this.tBoxLeave.TabIndex = 3;
+            this.tBoxJoin.Text = "Channel to join";
+            this.tBoxJoin.Enter += new System.EventHandler(this.tBoxJoin_Enter);
+            this.tBoxJoin.Leave += new System.EventHandler(this.tBoxJoin_Leave);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.rTBoxChat);
-            this.groupBox1.Location = new System.Drawing.Point(14, 180);
+            this.groupBox1.Location = new System.Drawing.Point(14, 194);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(307, 223);
+            this.groupBox1.Size = new System.Drawing.Size(307, 224);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chat";
             // 
-            // rTBoxChannels
+            // rTBoxChat
             // 
             this.rTBoxChat.Location = new System.Drawing.Point(7, 19);
-            this.rTBoxChat.Name = "rTBoxChannels";
+            this.rTBoxChat.Name = "rTBoxChat";
             this.rTBoxChat.ReadOnly = true;
+            this.rTBoxChat.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
             this.rTBoxChat.Size = new System.Drawing.Size(288, 198);
             this.rTBoxChat.TabIndex = 0;
+            this.rTBoxChat.TabStop = false;
             this.rTBoxChat.Text = "";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.rTBoxEvents);
-            this.groupBox2.Location = new System.Drawing.Point(13, 99);
+            this.groupBox2.Location = new System.Drawing.Point(327, 6);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(302, 75);
+            this.groupBox2.Size = new System.Drawing.Size(199, 412);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Event Logs";
             // 
             // rTBoxEvents
             // 
-            this.rTBoxEvents.Location = new System.Drawing.Point(9, 16);
+            this.rTBoxEvents.Location = new System.Drawing.Point(6, 19);
             this.rTBoxEvents.Name = "rTBoxEvents";
             this.rTBoxEvents.ReadOnly = true;
-            this.rTBoxEvents.Size = new System.Drawing.Size(287, 50);
+            this.rTBoxEvents.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rTBoxEvents.Size = new System.Drawing.Size(184, 385);
             this.rTBoxEvents.TabIndex = 0;
+            this.rTBoxEvents.TabStop = false;
             this.rTBoxEvents.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tBoxWhisper);
+            this.groupBox3.Controls.Add(this.cBoxJoinedChannels);
+            this.groupBox3.Controls.Add(this.rBtnWhisper);
+            this.groupBox3.Controls.Add(this.rBtnChat);
+            this.groupBox3.Controls.Add(this.rTBoxChatOutput);
+            this.groupBox3.Location = new System.Drawing.Point(14, 96);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(307, 94);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chat Input";
+            // 
+            // tBoxWhisper
+            // 
+            this.tBoxWhisper.Location = new System.Drawing.Point(168, 15);
+            this.tBoxWhisper.Name = "tBoxWhisper";
+            this.tBoxWhisper.Size = new System.Drawing.Size(133, 20);
+            this.tBoxWhisper.TabIndex = 4;
+            this.tBoxWhisper.Text = "Username to whisper";
+            this.tBoxWhisper.Visible = false;
+            this.tBoxWhisper.Enter += new System.EventHandler(this.tBoxWhisper_Enter);
+            this.tBoxWhisper.Leave += new System.EventHandler(this.tBoxWhisper_Leave);
+            // 
+            // rBtnWhisper
+            // 
+            this.rBtnWhisper.AutoSize = true;
+            this.rBtnWhisper.Location = new System.Drawing.Point(60, 19);
+            this.rBtnWhisper.Name = "rBtnWhisper";
+            this.rBtnWhisper.Size = new System.Drawing.Size(64, 17);
+            this.rBtnWhisper.TabIndex = 2;
+            this.rBtnWhisper.Text = "Whisper";
+            this.rBtnWhisper.UseVisualStyleBackColor = true;
+            this.rBtnWhisper.CheckedChanged += new System.EventHandler(this.rBtnWhisper_CheckedChanged);
+            // 
+            // rBtnChat
+            // 
+            this.rBtnChat.AutoSize = true;
+            this.rBtnChat.Checked = true;
+            this.rBtnChat.Location = new System.Drawing.Point(7, 20);
+            this.rBtnChat.Name = "rBtnChat";
+            this.rBtnChat.Size = new System.Drawing.Size(47, 17);
+            this.rBtnChat.TabIndex = 1;
+            this.rBtnChat.TabStop = true;
+            this.rBtnChat.Text = "Chat";
+            this.rBtnChat.UseVisualStyleBackColor = true;
+            this.rBtnChat.CheckedChanged += new System.EventHandler(this.rBtnChat_CheckedChanged);
+            // 
+            // rTBoxChatOutput
+            // 
+            this.rTBoxChatOutput.Location = new System.Drawing.Point(7, 42);
+            this.rTBoxChatOutput.Name = "rTBoxChatOutput";
+            this.rTBoxChatOutput.Size = new System.Drawing.Size(288, 46);
+            this.rTBoxChatOutput.TabIndex = 0;
+            this.rTBoxChatOutput.TabStop = false;
+            this.rTBoxChatOutput.Text = "";
+            this.rTBoxChatOutput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rTBoxChatOutput_KeyPress);
+            this.rTBoxChatOutput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.rTBoxChatOutput_KeyUp);
+            // 
+            // cBoxChannels
+            // 
+            this.cBoxChannels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxChannels.Enabled = false;
+            this.cBoxChannels.FormattingEnabled = true;
+            this.cBoxChannels.Location = new System.Drawing.Point(168, 21);
+            this.cBoxChannels.Name = "cBoxChannels";
+            this.cBoxChannels.Size = new System.Drawing.Size(133, 21);
+            this.cBoxChannels.TabIndex = 4;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tBoxJoin);
+            this.groupBox4.Controls.Add(this.cBoxChannels);
+            this.groupBox4.Controls.Add(this.btnJoin);
+            this.groupBox4.Controls.Add(this.btnLeave);
+            this.groupBox4.Location = new System.Drawing.Point(14, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(307, 84);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Join/Leave";
+            // 
+            // cBoxJoinedChannels
+            // 
+            this.cBoxJoinedChannels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxJoinedChannels.Enabled = false;
+            this.cBoxJoinedChannels.FormattingEnabled = true;
+            this.cBoxJoinedChannels.Location = new System.Drawing.Point(168, 15);
+            this.cBoxJoinedChannels.Name = "cBoxJoinedChannels";
+            this.cBoxJoinedChannels.Size = new System.Drawing.Size(133, 21);
+            this.cBoxJoinedChannels.TabIndex = 5;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 415);
+            this.ClientSize = new System.Drawing.Size(529, 422);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.tBoxLeave);
-            this.Controls.Add(this.tBoxJoin);
-            this.Controls.Add(this.btnLeave);
-            this.Controls.Add(this.btnJoin);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Tucxbot Open Source";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -140,11 +245,18 @@
         private System.Windows.Forms.Button btnJoin;
         private System.Windows.Forms.Button btnLeave;
         private System.Windows.Forms.TextBox tBoxJoin;
-        private System.Windows.Forms.TextBox tBoxLeave;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RichTextBox rTBoxChat;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RichTextBox rTBoxEvents;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rBtnWhisper;
+        private System.Windows.Forms.RadioButton rBtnChat;
+        private System.Windows.Forms.RichTextBox rTBoxChatOutput;
+        private System.Windows.Forms.TextBox tBoxWhisper;
+        private System.Windows.Forms.ComboBox cBoxChannels;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox cBoxJoinedChannels;
     }
 }
 
--- TucxbotForm/TucxbotForm/MainForm.cs
+++ TucxbotForm/TucxbotForm/MainForm.cs
@@ -15,11 +15,14 @@ namespace TucxbotForm
     {
         Twitch m_TwitchInstance;
         List<string> m_sJoinedChannels;
+        List<string> m_sPreviousChatMessages;
+        int m_iPreviousIndex;
         public MainForm()
         {
             InitializeComponent();
 
             m_sJoinedChannels = new List<string>();
+            m_sPreviousChatMessages = new List<string>();
             m_TwitchInstance = Twitch.Instance;
             m_TwitchInstance.OnChannelIRCConnected += OnChannelIRCConnected;
             m_TwitchInstance.OnChannelInput += OnChannelInput;
@@ -103,29 +106,132 @@ namespace TucxbotForm
                     m_sJoinedChannels.Add(sNewText);
                     m_TwitchInstance.JoinChannel(sNewText);
                     btnLeave.Enabled = true;
+
+                    cBoxChannels.Items.Add(sNewText);
+                    cBoxJoinedChannels.Items.Add(sNewText);
+                    cBoxChannels.Enabled = true;
+                    cBoxJoinedChannels.Enabled = true;
+
+                    if (cBoxChannels.Items.Count == 1)
+                    {
+                        cBoxChannels.SelectedIndex = 0;
+                    }
+                    if (cBoxJoinedChannels.Items.Count == 1)
+                    {
+                        cBoxJoinedChannels.SelectedIndex = 0;
+                    }
                 }
                 tBoxJoin.Clear();
-
-                
             }
         }
 
         private void btnLeave_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(tBoxLeave.Text))
-            {
-                string sNewText = tBoxLeave.Text.Trim().ToLower();
-                if (m_sJoinedChannels.Contains(sNewText))
-                {
-                    m_sJoinedChannels.Remove(sNewText);
-                    m_TwitchInstance.LeaveChannel(sNewText);
+            string sChannel = cBoxChannels.Items[cBoxChannels.SelectedIndex].ToString();
+            m_TwitchInstance.LeaveChannel(sChannel);
 
-                    if (m_sJoinedChannels.Count == 0)
-                    {
-                        btnLeave.Enabled = false;
-                    }
+            cBoxChannels.Items.Remove(sChannel);
+            cBoxJoinedChannels.Items.Remove(sChannel);
+            m_sJoinedChannels.Remove(sChannel);
+
+
+            if (cBoxChannels.Items.Count == 0)
+            {
+                cBoxChannels.Enabled = false;
+            }
+            else
+            {
+                cBoxChannels.SelectedIndex = 0;
+            }
+
+            if (cBoxJoinedChannels.Items.Count == 0)
+            {
+                cBoxJoinedChannels.Enabled = false;
+            }
+            else
+            {
+                cBoxJoinedChannels.SelectedIndex = 0;
+            }
+        }
+
+        private void rBtnChat_CheckedChanged(object sender, EventArgs e)
+        {
+            cBoxJoinedChannels.Visible = true;
+            tBoxWhisper.Visible = false;
+            tBoxWhisper.Text = "Username to whisper";
+        }
+
+        private void rBtnWhisper_CheckedChanged(object sender, EventArgs e)
+        {
+            cBoxJoinedChannels.Visible = false;
+            tBoxWhisper.Visible = true;
+            cBoxChannels.Text = "Select a channel";
+        }
+
+        private void tBoxWhisper_MouseClick(object sender, MouseEventArgs e)
+        {
+            tBoxWhisper.Clear();
+        }
+
+        private void tBoxWhisper_Enter(object sender, EventArgs e)
+        {
+            tBoxWhisper.Clear();
+        }
+
+        private void tBoxWhisper_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tBoxWhisper.Text))
+            {
+                tBoxWhisper.Text = "Username to whisper";
+            }
+        }
+
+        private void tBoxJoin_Enter(object sender, EventArgs e)
+        {
+            tBoxJoin.Clear();
+        }
+
+        private void tBoxJoin_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tBoxJoin.Text))
+            {
+                tBoxJoin.Text = "Channel to join";
+            }
+        }
+
+        private void rTBoxChatOutput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return && !string.IsNullOrEmpty(rTBoxChatOutput.Text))
+            {
+                string sMessage = rTBoxChatOutput.Text.Trim();
+                m_sPreviousChatMessages.Add(sMessage);
+                rTBoxChatOutput.Clear();
+
+                string sTarget = (rBtnChat.Checked ? cBoxJoinedChannels.SelectedItem.ToString() : tBoxWhisper.Text);
+
+                if (rBtnChat.Checked)
+                {
+                    m_TwitchInstance.SendMessage(sTarget, sMessage);
                 }
-                tBoxLeave.Clear();
+                else
+                {
+                    m_TwitchInstance.SendWhisper(sTarget, sMessage);
+                }
+                
+            }
+            
+        }
+
+        private void rTBoxChatOutput_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && m_sPreviousChatMessages.Count > 0)
+            {
+                if (m_iPreviousIndex == m_sPreviousChatMessages.Count)
+                {
+                    m_iPreviousIndex -= 1;
+                }
+                rTBoxChatOutput.Text = m_sPreviousChatMessages[m_sPreviousChatMessages.Count - 1 - m_iPreviousIndex];
+                ++m_iPreviousIndex;
             }
         }
     }
