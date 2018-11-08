868c63a26dd6054809c6df58340f507539e1983e
Sirtucx <curtiscabral@gmail.com>
2018-11-07 21:30:38 -0500

    - Added more messages for sub gifts.

--- SubscriberNotificationMod/SubscriberNotificationMod/SubscriberNotification.cs
+++ SubscriberNotificationMod/SubscriberNotificationMod/SubscriberNotification.cs
@@ -19,33 +19,61 @@ namespace SubscriberNotificationMod
             }
             string sMessage = $"MrDestructoid Thank you {usernotice.Username} for";
 
-            switch (usernotice.SubPlan)
+            if (usernotice.MessageID != UserNotice.SubscriptionType.Gift)
             {
-                case UserNotice.SubscriptionPlan.Prime:
-                    {
-                        sMessage = $"{sMessage} subscribing to the channel using Twitch Prime";
-                        break;
-                    }
-                case UserNotice.SubscriptionPlan.Tier1:
-                    {
-                        sMessage = $"{sMessage} subscribing to the channel";
-                        break;
-                    }
-                case UserNotice.SubscriptionPlan.Tier2:
-                    {
-                        sMessage = $"{sMessage} becoming the SUPER SHINY SUB";
-                        break;
-                    }
-                case UserNotice.SubscriptionPlan.Tier3:
-                    {
-                        sMessage = $"{sMessage} becoming the ULTIMATE SUB";
-                        break;
-                    }
-            }
+                switch (usernotice.SubPlan)
+                {
+                    case UserNotice.SubscriptionPlan.Prime:
+                        {
+                            sMessage = $"{sMessage} subscribing to the channel using Twitch Prime";
+                            break;
+                        }
+                    case UserNotice.SubscriptionPlan.Tier1:
+                        {
+                            sMessage = $"{sMessage} subscribing to the channel";
+                            break;
+                        }
+                    case UserNotice.SubscriptionPlan.Tier2:
+                        {
+                            sMessage = $"{sMessage} becoming the SUPER SHINY SUB";
+                            break;
+                        }
+                    case UserNotice.SubscriptionPlan.Tier3:
+                        {
+                            sMessage = $"{sMessage} becoming the ULTIMATE SUB";
+                            break;
+                        }
+                }
 
-            if (usernotice.ResubConsecutiveMonths > 1)
+                if (usernotice.ResubConsecutiveMonths > 1)
+                {
+                    sMessage = $"{sMessage} and thank you for remaining subscribed for {usernotice.ResubConsecutiveMonths} months! MrDestructoid";
+                }
+            }
+            else
             {
-                sMessage = $"{sMessage} and thank you for remaining subscribed for {usernotice.ResubConsecutiveMonths} months! MrDestructoid";
+                sMessage = $"{sMessage} gifting {usernotice.GiftedSubscriptionRecipientName}";
+
+                switch (usernotice.SubPlan)
+                {
+                    case UserNotice.SubscriptionPlan.Tier1:
+                        {
+                            sMessage = $"{sMessage} a sub!";
+                            break;
+                        }
+                    case UserNotice.SubscriptionPlan.Tier2:
+                        {
+                            sMessage = $"{sMessage} a SUPER SHINY SUB!";
+                            break;
+                        }
+                    case UserNotice.SubscriptionPlan.Tier3:
+                        {
+                            sMessage = $"{sMessage} the ULTIMATE SUB!";
+                            break;
+                        }
+                }
+
+                sMessage = $"{sMessage} {usernotice.Username} has gifted a total of {usernotice.GiftedSubscriptionsCount} subs in this channel! MrDestructoid";
             }
 
             m_TwitchClient.SendChatMessage(usernotice.Channel, sMessage);
